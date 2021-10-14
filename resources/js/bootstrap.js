60c8149f1b283db4d95c50a6b04db3ffa1d91010
Hatim Lagzar <lagzarhatim@gmail.com>
2021-10-14 20:48:50 +0100

    Create prdouct frontend

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,3 @@
 require('@popperjs/core')
 require('bootstrap')
-window.VueRouter = require('vue-router')
+window.toastr = require('toastr')
--- resources/js/pages/Products/Create.vue
+++ resources/js/pages/Products/Create.vue
@@ -1,9 +1,62 @@
 <template>
-	<h1>Create Product</h1>
+  <h1>Create Product</h1>
+
+  <form @submit="handleCreateProduct">
+    <div class="form-group mb-2">
+      <label for="name">Name</label>
+      <input name="name" id="name" type="text" class="form-control" />
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="price">Price</label>
+      <input name="price" id="price" type="text" class="form-control" />
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="description">Description</label>
+      <textarea
+        name="description"
+        id="description"
+        class="form-control"
+      ></textarea>
+    </div>
+
+    <div class="form-group mb-2">
+      <label for="description" class="form-label">Product Thumbnail</label>
+      <input
+        name="image"
+        class="form-control"
+        type="file"
+        id="description"
+        accept="image/*"
+      />
+    </div>
+
+    <div class="form-group mb-2">
+      <button type="submit" class="btn btn-primary me-2">Validate</button>
+      <button type="reset" class="btn btn-light">Clear Fields</button>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-	setup: () => ({})
-}
+  setup: () => ({}),
+
+  methods: {
+    handleCreateProduct(e) {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      fetch("/api/products", { method: "POST", body: formData })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.status === 200) {
+            toastr.error(response.msg);
+          } else {
+            toastr.error(response.msg);
+          }
+        });
+    },
+  },
+};
 </script>
