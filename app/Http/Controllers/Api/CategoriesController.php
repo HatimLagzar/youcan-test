92940f5b6d0b8c0c9cbbad3fd436c0645a6136f8
Hatim Lagzar <lagzarhatim@gmail.com>
2021-10-14 20:18:35 +0100

    Api Categories | Retrieve Categories when filter component is mounted

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+	public function all()
+	{
+		return response([
+			'status' => 200,
+			'msg' => 'Categories pulled successfully.',
+			'categories' => Category::all()
+		]);
+	}
+}
--- resources/js/components/CategoryFilter.vue
+++ resources/js/components/CategoryFilter.vue
@@ -3,6 +3,7 @@
 		<label for="category">Category</label>
 		<select class="form-select w-auto d-inline-block ms-2 bg-transparent border-0" id="category">
 			<option value="">All</option>
+			<option v-for="category in categories" value="{{ category.id }}">{{ category.name }}</option>
 		</select>
 	</div>
 </template>
@@ -10,5 +11,24 @@
 <script>
 export default {
 	setup: () => ({}),
+	data: () => {
+		return {
+			categories: []
+		}
+	},
+	mounted() {
+		this.getCategories()
+	},
+	methods: {
+		getCategories() {
+			return fetch('/api/categories')
+				.then(response => response.json())
+				.then(response => {
+					console.log(this)
+					this.categories = response.categories
+				})
+				.catch(error => console.log(error))
+		}
+	}
 };
 </script>
