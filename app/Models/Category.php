efe5686073c0829a86f63c6b876755e857b3e463
Hatim Lagzar <lagzarhatim@gmail.com>
2021-10-14 10:55:08 +0100

    Create the categories table migration and Category Model with parent relationship

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * Class Category
+ * @package App\Models
+ * @property int $id
+ * @property string $name
+ * @property int $parent_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Category extends Model
+{
+	use HasFactory;
+
+	protected $table = 'categories';
+
+	/**
+	 * Get the parent category
+	 */
+	public function parentCategory()
+	{
+		return $this->hasOne(Category::class, 'id', 'parent_id');
+	}
+}
