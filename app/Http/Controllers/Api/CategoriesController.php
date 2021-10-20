78e7a0a4cf0239d346d3db5ff557c9a7eeff9a11
Hatim Lagzar <lhatimdev@gmail.com>
2021-10-20 16:49:28 +0100

    Use CategoryService to retrive categories from controller

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
+use App\Services\CategoryService;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
+    protected CategoryService $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
     public function all()
     {
         return response([
             'status' => 200,
             'msg' => 'Categories pulled successfully.',
-            'categories' => Category::all()
+            'categories' => $this->categoryService->getAll()
         ]);
     }
 }
