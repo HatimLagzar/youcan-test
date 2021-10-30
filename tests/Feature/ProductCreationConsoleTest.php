38c92ee5dd16bc557d2b64cc5acc9653ff3fb67a
Hatim Lagzar <lhatimdev@gmail.com>
2021-10-30 19:39:39 +0100

    Add validation to product creation from console

---
+++ tests/Feature/ProductCreationConsoleTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class ProductCreationConsoleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_create_product_console()
+    {
+        $productCategoriesChoices = Category::all('name')->toArray();
+        $productCategoriesChoices = ['None', ...Arr::flatten($productCategoriesChoices)];
+
+        $this->artisan('product:create')
+            ->expectsQuestion('Enter product name', 'T-Shirt')
+            ->expectsQuestion('Enter product description', 'description')
+            ->expectsQuestion('Enter product price (Number)', 39.99)
+            ->expectsQuestion(
+                'Enter product image, URL or local path',
+                'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+            )
+            ->expectsChoice(
+                'Select product categories, to use multiple categories use comma (Cat One, Cat Five)...',
+                ['None'],
+                $productCategoriesChoices
+            )
+            ->assertExitCode(Command::SUCCESS);
+    }
+}
