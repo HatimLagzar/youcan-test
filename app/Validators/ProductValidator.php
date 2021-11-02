792c05a365b1d6e905393cc3727de21a956793c6
Hatim Lagzar <lhatimdev@gmail.com>
2021-11-02 15:47:26 +0100

    remove the use of Validator Facade and instead use dependency injection with Validation Factory

--- app/Validators/ProductValidator.php
+++ app/Validators/ProductValidator.php
@@ -3,17 +3,24 @@
 namespace App\Validators;
 
 use App\Exceptions\ValidationException;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Factory as Validator;
 
 class ProductValidator
 {
+    private Validator $validator;
+
+    public function __construct(Validator $validator)
+    {
+        $this->validator = $validator;
+    }
+
     /**
      * Validate the inputs and throw and exception if the validation fails
      * @throws ValidationException
      */
     public function validate(array $inputs)
     {
-        $validation = Validator::make($inputs, $this->rules());
+        $validation = $this->validator->make($inputs, $this->rules());
 
         if ($validation->fails()) {
             throw new ValidationException($validation);
